50c1b73eee39d30c4e8e525e45351627e272a9b4
AndriiBai <andriy.bay@gmail.com>
2019-03-22 07:41:41 +0200

    add code

--- js/functions.js
+++ js/functions.js
@@ -9,6 +9,7 @@ var sliderProto = function(sliderParams){
     this.slideEffect = sliderParams.slideEffect;
     this.slideMargin = sliderParams.slideMargin;
     this.autoSlide = sliderParams.autoSlide;
+    this.stopOnHover = sliderParams.stopOnHover;
     this.activeArrows = sliderParams.activeArrows;
     this.showArrows = sliderParams.showArrows;
     this.showBullets = sliderParams.showBullets;
@@ -23,25 +24,25 @@ var sliderProto = function(sliderParams){
  * arguments: slideMargin property
  * @return int
  */
-    sliderProto.prototype.slideWidth = function(){
-        if(this.slideMargin){
-            if($(window).width() > 991){
-                return (100/this.activeSlidesL) - this.slideMargin;
-            }else{
-                return 0;
-            }
+sliderProto.prototype.slideWidth = function(){
+    if(this.slideMargin){
+        if($(window).width() > 991){
+            return (100/this.activeSlidesL) - this.slideMargin;
         }else{
-            if($(window).width() > 991){
-                return (100/this.activeSlidesL);
-            }else{
-                return 0;
-            }
+            return 0;
         }
-    };
+    }else{
+        if($(window).width() > 991){
+            return (100/this.activeSlidesL);
+        }else{
+            return 0;
+        }
+    }
+};
 
 /**
-*	Push all items of slider in array
-*/
+ *	Push all items of slider in array
+ */
 sliderProto.prototype.slide = function(){
     var sliderContainer = $('#'+this.id);
     var allSlidersItems = $(sliderContainer).find('.item');
@@ -55,9 +56,18 @@ sliderProto.prototype.slide = function(){
     }
 };
 
+sliderProto.prototype.removeAllActiveItems = function (){
+    $('#'+this.id).find('.item').removeClass(''+this.activeClass).addClass(''+this.hiddenClass).removeAttr('style');
+    $('.navigation').find('.dot').removeClass(''+this.activeClass);
+};
+
+sliderProto.prototype.hoverDetector = function(entity){
+// check if entity is on hover
+// return bool
+};
+
 sliderProto.prototype.setSlides = function(arr){
     $(arr).addClass(''+this.activeClass).removeClass(''+this.hiddenClass);
-    //$(arr).css({'width':this.slideWidth()+'%'});
     if(this.slideMargin){
         $(arr).css({'width': this.slideWidth()+'%','margin-right':this.slideMargin/2+'%','margin-left':this.slideMargin/2+'%'});
     }else{
@@ -66,21 +76,21 @@ sliderProto.prototype.setSlides = function(arr){
 };
 
 sliderProto.prototype.setBullets = function(){
-	if(this.showBullets === true) {
+    if(this.showBullets === true) {
         var finalQuantityOfSlides = 0;
-		$('#'+this.id).append("<div class='navigation'></div>");
-		var numberSlidesPortion = parseInt(this.slidesQuantity/this.activeSlidesL);
-		var numberNotFullPortion = this.slidesQuantity - numberSlidesPortion * this.activeSlidesL;
-		if(numberNotFullPortion){
+        $('#'+this.id).append("<div class='navigation'></div>");
+        var numberSlidesPortion = parseInt(this.slidesQuantity/this.activeSlidesL);
+        var numberNotFullPortion = this.slidesQuantity - numberSlidesPortion * this.activeSlidesL;
+        if(numberNotFullPortion){
             finalQuantityOfSlides = numberSlidesPortion + 1;
-		}else{
+        }else{
             finalQuantityOfSlides = numberSlidesPortion;
         }
         for (var d = 0; d < finalQuantityOfSlides; d++) {
             $('.navigation').append("<div class='dot'></div>");
         }
-		var allSlidersDots = $('.navigation').find('.dot');
-		$(allSlidersDots[0]).addClass(''+this.activeClass);
+        var allSlidersDots = $('.navigation').find('.dot');
+        $(allSlidersDots[0]).addClass(''+this.activeClass);
     }
 };
 
@@ -101,20 +111,21 @@ sliderProto.prototype.touchBullets = function(){
     var allSlidersDots = $('.navigation').find('.dot');
 
     $(allSlidersDots).click(function () {
-        $(allSlidersDots).removeClass('active');
+        $(allSlidersDots).removeClass(''+self.activeClass);
         $(this).addClass(''+self.activeClass);
     });
 };
 
 sliderProto.prototype.setActiveSlides = function(){
-	var sliderContainer = $('#'+this.id);
+    var sliderContainer = $('#'+this.id);
     var allSlidersItems = $(sliderContainer).find('.item');
-	for(var j = 0; j < this.activeSlidesL; j++){
+    for(var j = 0; j < this.activeSlidesL; j++){
         $(allSlidersItems[j]).addClass(''+this.activeClass).removeClass(''+this.hiddenClass);
         this.setSlides(this.arr[this.counter]);
         this.counter++;
     }
 };
+
 
 sliderProto.prototype.nextArrow = function(){
     var self = this;
@@ -161,6 +172,18 @@ sliderProto.prototype.prevArrow = function(){
         if (self.counter <= 0 ) {
             self.counter = self.arr.length;
         }
+
+        console.log(self.counter);
+    });
+};
+
+
+/*---------- Auto running of slider ---------------- */
+sliderProto.prototype.run = function(){
+    if(this.autoSlide === true && this.slideEffect && this.stopOnHover === true){
+        var allSlidersDots = $('.navigation').find('.dot');
+        this.removeAllActiveItems();
+
     });
 };
 
@@ -178,9 +201,7 @@ sliderProto.prototype.run = function(){
             this.setSlides(this.arr[this.counter]);
             this.counter++;
         }
-
         $(allSlidersDots[this.counter/this.activeSlidesL - 1]).addClass(''+this.activeClass);
-
         if(this.counter >= this.arr.length) {
             this.counter = 0;
         }
@@ -193,6 +214,8 @@ sliderProto.prototype.init = function(){
     this.setArrows();
     //this.touchBullets();
     this.setActiveSlides();
+    // this.nextArrow();
+    // this.prevArrow();
     //this.nextArrow();
     //this.prevArrow();
 };
@@ -200,6 +223,7 @@ sliderProto.prototype.init = function(){
 $(document).ready(function(){
     var sliderParams = {
         id: 'slider_rw',
+        activeSlidesL: 3,
         activeSlidesL: 4,
         activeSlidesM: 3,
         activeSlidesS: 2,
@@ -208,6 +232,7 @@ $(document).ready(function(){
         slideEffect: 'fadeIn',
         slideMargin: 2,
         autoSlide: true,
+        stopOnHover: true,
         showArrows: false,
         showBullets: true,
         step: 1
